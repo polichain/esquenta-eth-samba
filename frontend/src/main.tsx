910ca2e5a329e21a784229dcaa71dcda8a9ed2fa
otaviootavio <otaviovacari@usp.br>
2024-03-11 14:14:23 -0300

    first commit

---
+++ frontend/src/main.tsx
@@ -0,0 +1,29 @@
+import { Buffer } from "node:buffer";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { WagmiProvider } from "wagmi";
+
+import App from "./App.tsx";
+import { config } from "./wagmi.ts";
+
+import "./index.css";
+
+globalThis.Buffer = Buffer;
+
+const queryClient = new QueryClient();
+
+const rootElement = document.getElementById("root");
+if (rootElement === null) {
+  console.error("Failed to find the root element");
+} else {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </WagmiProvider>
+    </React.StrictMode>,
+  );
+}
